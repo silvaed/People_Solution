aa816f539e426dd638ebb9f0e6b92ba1e117e44f
silvaed <edsonbr27@gmail.com>
2019-01-23 21:48:53 +0000

    Commit_2

--- People/People/Startup.cs
+++ People/People/Startup.cs
@@ -15,6 +15,7 @@ namespace People
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,6 +27,7 @@ namespace People
             }
 
             app.UseStaticFiles();
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
